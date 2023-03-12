9e4777dd37aec85a0925d0bcb4f5f94fc06c21e7
Robert Kieffer <robert@broofa.com>
2023-03-12 09:37:31 -0700

    chore: lint:fix

--- test/examples/contexts.md
+++ test/examples/contexts.md
@@ -1,21 +1,23 @@
 ```javascript --run
-const foo = 'def'
+const foo = 'def';
 ```
 
 ```javascript --run alpha
-const foo = 'alpha'
+const foo = 'alpha';
 ```
 
 ```javascript --run beta
-const foo = 'beta'
+const foo = 'beta';
 ```
 
 ```javascript --run
-const def = typeof(foo); // RESULT
+const def = typeof foo; // RESULT
 ```
+
 ```javascript --run alpha
 const alpha = foo; // RESULT
 ```
+
 ```javascript --run beta
 const beta = foo; // RESULT
 ```
--- test/test.spec.js
+++ test/test.spec.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { render } = require('..');
 const test = require('ava');
 
-function transform (filename) {
+function transform(filename) {
   const filepath = path.join(__dirname, 'examples', filename);
   const inputText = fs.readFileSync(filepath, 'utf8');
   return render(inputText, {
@@ -12,25 +12,25 @@ function transform (filename) {
   });
 }
 
-test('basic', t => {
+test('basic', (t) => {
   const md = transform('basic.md');
   t.regex(md, /^# Header/m, 'has header');
   t.regex(md, /res =.*2$/m, 'has result');
   t.regex(md, /^# Footer/m, 'has footer');
 });
 
-test('module', t => {
+test('module', (t) => {
   const md = transform('module.md');
   t.regex(md, /res =.*123$/m, 'has access to module exports');
 });
 
-test('es6', t => {
+test('es6', (t) => {
   const md = transform('es6.md');
   t.regex(md, /fooRes =.*123$/m, 'has access to module exports');
   t.regex(md, /barRes =.*123$/m, 'has access to module exports');
 });
 
-test('contexts', t => {
+test('contexts', (t) => {
   const md = transform('contexts.md');
   t.regex(md, /def =.*undefined/m, 'foo in unnamed context is not undefined');
   t.regex(md, /alpha =.*alpha/m, 'foo in alpha context');
